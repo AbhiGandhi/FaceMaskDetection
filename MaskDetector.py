c57d8375745be0180a875debfb8f76db829badc5
AbhiGandhi <58977382+AbhiGandhi@users.noreply.github.com>
2020-05-24 16:31:41 +0530

    Sound Alert

--- MaskDetector.py
+++ MaskDetector.py
@@ -6,6 +6,12 @@ from utils.anchor_decode import decode_bbox
 from utils.nms import single_class_non_max_suppression
 from PIL import Image, ImageDraw, ImageFont
 #import caffe
+import pyglet
+
+music = pyglet.resource.media('music.mp3')
+music.play()
+
+pyglet.app.run()
 
 #||FROM HERE DONT MESS WITH CODE Bois
 
@@ -76,6 +82,7 @@ def inference(net, image, conf_thresh=0.5, iou_thresh=0.4, target_shape=(160, 16
             else:
                 cv2.putText(image, "%s: %.2f" % (id2class[class_id], conf), (xmin + 2, ymin - 2),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.8, colors[class_id])
+                pyglet.app.run()   #Sound Alert
     return image
 
 def run_on_video(Net, video_path, conf_thresh=1.0):
